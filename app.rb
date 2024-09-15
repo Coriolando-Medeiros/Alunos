9f3d0b26a89e8d28809808e18aed9753a0527510
Coriolando-Medeiros <coriolando.medeiros@gmail.com>
2024-09-15 10:57:17 -0300

    style(menu): adequação do menu

--- app.rb
+++ app.rb
@@ -5,15 +5,19 @@ class App
         gerenciar_curso = Curso.new
         gerenciar_aluno = Aluno.new
 
-        puts "Menu"
 
         loop do
+            puts ""
+            puts "Menu"
+            puts ""
             puts "1 - Cadastrar curso"
-            puts "2 - Remover curso"
+            puts "2 - Listar cursos"
+            puts "3 - Remover curso"
             puts "---------------------"
-            puts "3 - Cadastrar aluno"
-            puts "4 - Listar aluno"
+            puts "4 - Cadastrar aluno"
+            puts "5 - Listar aluno"
             puts "0 - Sair"
+            puts ""
             print "Opção: "
 
             escolha = gets.chomp.to_i
@@ -22,10 +26,12 @@ class App
             if escolha == 1
                 gerenciar_curso.cadastrar_curso
             elsif escolha == 2
-                gerenciar_curso.remover_curso
+                gerenciar_curso.listar_cursos
             elsif escolha == 3
-                gerenciar_aluno.cadastrar_aluno
+                gerenciar_curso.remover_curso
             elsif escolha == 4
+                gerenciar_aluno.cadastrar_aluno
+            elsif escolha == 5
                 gerenciar_aluno.listar_aluno
             elsif escolha == 0
                 puts "Saindo..."
