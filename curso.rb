f507604836f4d1a3f0f93c33f4ab7d35bde455e9
Coriolando-Medeiros <coriolando.medeiros@gmail.com>
2024-09-15 10:59:21 -0300

    feat: adicionar a opção de listar e remover curso mostrarem que não há cursos cadastrados

--- curso.rb
+++ curso.rb
@@ -17,21 +17,42 @@ class Curso
 
     def listar_cursos
         puts "Lista de Cursos"
-        @cursos.each_with_index do |curso, indice|
-            puts "#{indice +1}º - Curso: #{curso[:curso]} - Professor: #{curso[:professor]} - Carga horária: #{curso[:carga_horaria]}"
+        if @cursos.empty?
+            puts "Não há cursos cadastrados"
+            puts "Cadastrar novo curso?"
+            puts "1 - Sim | 0 - Não"
+            print "Opção: "
+            escolha = gets.chomp.to_i
+
+            if escolha == 1
+                cadastrar_curso
+            elsif escolha == 0
+                puts "Saindo..."
+                return
+            else
+                puts "Opção inválida!"
+            end
+        else
+            @cursos.each_with_index do |curso, indice|
+                puts "#{indice +1}º - Curso: #{curso[:curso]} - Professor: #{curso[:professor]} - Carga horária: #{curso[:carga_horaria]}"
+            end
         end
     end
 
     def remover_curso
         puts "Remover Curso"
-        @cursos.each_with_index do |curso, indice|
-            puts "#{indice + 1}º - Curso: #{curso[:curso]} - Professor: #{curso[:professor]} - Carga horária: #{curso[:carga_horaria]}"
+        if @cursos.empty?
+            puts "Não há cursos cadastrados"
+        else
+            @cursos.each_with_index do |curso, indice|
+                puts "#{indice + 1}º - Curso: #{curso[:curso]} - Professor: #{curso[:professor]} - Carga horária: #{curso[:carga_horaria]}"
+            end
+            puts "Escolha o curso a ser removido"
+            puts "Digite o número do índice"
+            print "Indice: "
+            indice = gets.chomp.to_i - 1
+            @cursos.delete_at(indice)
+            puts "Curso Removido!"
         end
-        puts "Escolha o curso a ser removido"
-        puts "Digite o número do índice"
-        print "Indice: "
-        indice = gets.chomp.to_i - 1
-        @cursos.delete_at(indice)
-        puts "Curso Removido!"
     end
 end
